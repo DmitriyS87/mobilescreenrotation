ac5e25e4172655a7bd94a6af779171b5c19c6b95
DmitriyS87 <schukindmitriy@gmail.com>
2019-08-01 00:49:20 +0800

    updaates images

--- src/components/full-screen-picture.jsx
+++ src/components/full-screen-picture.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledContainer = styled.picture`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   img {
-    max-width: 100vw;
-    max-height: 100vh;
+    max-width: 100%;
+    max-height: 100%;
   }
 `;
 
@@ -12,11 +17,11 @@ const FullScreenPicture = props => {
   const { pictureTitle } = props;
   return (
     <StyledContainer>
-      <source srcSet={`img/squooshed/${pictureTitle}.webp`} type="image/webp" />
+      <source srcSet={`img/squooshed/${pictureTitle}_min.webp 1x, img/squooshed/${pictureTitle}_mid.webp 2x, img/squooshed/${pictureTitle}_max.webp 3x`} type='image/webp' />
+      <source srcSet={`img/squooshed/${pictureTitle}_min.jpg 1x, img/squooshed/${pictureTitle}_mid.jpg 2x, img/squooshed/${pictureTitle}_max.jpg 3x`} type='image/jpeg' />
       <img
-        src={`img/squooshed/${pictureTitle}.jpg`}
-        srcSet={`img/squooshed/${pictureTitle}.png 2x, img/${pictureTitle}.png 3x`}
-        alt=""
+        src={`img/squooshed/${pictureTitle}_min.png`}
+        alt="Красивая картинка"
       />
     </StyledContainer>
   );
