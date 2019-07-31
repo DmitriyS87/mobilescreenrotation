23f8f9c40a0ddfa21bae9cfb4e201cfee26990c8
DmitriyS87 <schukindmitriy@gmail.com>
2019-07-31 22:26:04 +0800

    changes document to window.document for more browsers compatibility

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
 
-const entryPoint = document.querySelector('.app');
+const entryPoint = window.document.querySelector(".app");
 
 const init = () => {
   ReactDOM.render(<App />, entryPoint);
